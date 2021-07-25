03977166cdf0fbeea79b35c9da10ba984fe75d8c
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2021-07-25 21:18:52 +0300

    Add style and JS

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+const cards = document.querySelectorAll(".card");
+window.addEventListener("resize", () => {
+    console.log(window.innerHeight);
+});
 
+document.addEventListener("scroll", displayCards);
+
+let a = cards[0].getClientRects()[0].top;
+let b = cards[0].getBoundingClientRect().top;
+console.log(a, b);
+
+function displayCards() {
+    const height = (window.innerHeight * 4) / 5;
+
+    cards.forEach((card) => {
+        if (card.getBoundingClientRect().top < height) {
+            card.classList.add("card-display");
+        } else {
+            card.classList.remove("card-display");
+        }
+    });
+}
+
+displayCards();
